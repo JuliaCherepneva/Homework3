bc9121c0827e9e939538682b2c3d9f53d0bf34c3
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-04-19 23:06:22 +0400

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,48 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        byte boxOne = 15;
+        System.out.println("Значение переменной" + " " + "boxOne" + " " + "с типом" + " " + "byte" + " " + "равно" + " " + boxOne + ".");
+        short boxTwo = 15000;
+        System.out.println("Значение переменной" + " " + "boxTwo" + " " + "с типом" + " " + "short" + " " + "равно" + " " + boxTwo + ".");
+        int boxThree = 150000;
+        System.out.println("Значение переменной" + " " + "boxThree" + " " + "с типом" + " " + "int" + " " + "равно" + " " + boxThree + ".");
+        long box4 = 155555555555L;
+        System.out.println("Значение переменной" + " " + "box4" + " " + "с типом" + " " + "long" + " " + "равно" + " " + box4 + ".");
+        float box5 = 15.555555f;
+        System.out.println("Значение переменной" + " " + "box5" + " " + "с типом" + " " + "float" + " " + "равно" + " " + box5 + ".");
+        double box6 = 1.555645767567564;
+        System.out.println("Значение переменной" + " " + "box6" + " " + "с типом" + " " + "double" + " " + "равно" + " " + box6 + ".");
+        System.out.println(" ");
+
+        System.out.println("Задание 2");
+        float one = 27.12f;
+        System.out.println("float" + " " + "one" + " " + "=" + " " + one);
+        long two = 987678965549L;
+        System.out.println("long" + " " + "two" + " " + "=" + " " + two);
+        double three = 2.786;
+        System.out.println("double" + " " + "three" + " " + "=" + " " + three);
+        short four = 569;
+        System.out.println("short" + " " + "four" + " " + "=" + " " + four);
+        short five = -159;
+        System.out.println("short" + " " + "five" + " " + "=" + " " + five);
+        int six = 27897;
+        System.out.println("int" + " " + "six" + " " + "=" + " " + six);
+        byte seven = 67;
+        System.out.println("byte" + " " + "seven" + " " + "=" + " " + seven);
+
+        System.out.println("Задание 3");
+        byte teacherLp = 23;
+        byte teacherAs = 27;
+        byte teacherEa = 30;
+        short totalPaper = 480;
+        int totalStudents = teacherLp + teacherAs + teacherEa;
+        int paper = totalPaper / totalStudents;
+        System.out.println("На каждого ученика рассчитано" + " " + paper + " " + "листов бумаги.");
+
+        System.out.println("Задание 4");
+
 
     }
 }
