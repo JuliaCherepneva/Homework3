57e9ba0f89b1efd1be192f649d275f7be11169f6
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-04-20 15:16:26 +0400

    Шесть задач

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("Задание 2");
         float one = 27.12f;
         System.out.println("float" + " " + "one" + " " + "=" + " " + one);
-        long two = 987678965549L;
+        long two = 987_678_965_549L;
         System.out.println("long" + " " + "two" + " " + "=" + " " + two);
         double three = 2.786;
         System.out.println("double" + " " + "three" + " " + "=" + " " + three);
@@ -55,13 +55,31 @@ public class Main {
         System.out.println("За 20 минут машина произвела" + " " + machinePerformanceIn20Minutes + " " + "штук бутылок");
         int machinePerformanceIn1day = machinePerformanceIn1Minutes * minutesInOneDay;
         System.out.println("За сутки машина произвела" + " " + machinePerformanceIn1day + " " + "штук бутылок");
-        int ThreeDays = machinePerformanceIn1day * 3;
-        System.out.println("За 3 дня машина произвела" + " " + ThreeDays + " " + "штук бутылок");
+        int threeDays = machinePerformanceIn1day * 3;
+        System.out.println("За 3 дня машина произвела" + " " + threeDays + " " + "штук бутылок");
         int oneMonth = machinePerformanceIn1day * daysInOneMonth;
         System.out.println("За месяц машина произвела" + " " + oneMonth + " " + "штук бутылок");
         System.out.println(" ");
 
         System.out.println("Задание 5");
+        byte totalCans = 120;
+        byte white = 2;
+        byte brown = 4;
+        int totalPaint = white + brown;
+        int totalClass = totalCans / totalPaint;
+        // Cчитаем сколько нужно белой и коричневой краски
+        int white1 = white * totalClass;
+        int brown1 = brown * totalClass;
+        System.out.println("В школе, где" + " " + totalClass + " " + "классов, нужно" + " " + white1 + " " + "банок белой краски и" + " " + brown1 + " " + "банок коричневой краски");
+        System.out.println(" ");
 
+        System.out.println("Задание 6");
+        int bananaInGrams = 5 * 80;
+        int milkInGram = (200 * 105) / 100;
+        int iceCreamInGram = 2 * 100;
+        int eggInGram = 4 * 70;
+        double totalGram = bananaInGrams + milkInGram + iceCreamInGram + eggInGram;
+        double totalKg = totalGram / 1000;
+        System.out.println("Завтрак состовляет" + " " + totalGram + " " + "г." + " " + "или" + " " + totalKg + " " + "кг.");
     }
 }
